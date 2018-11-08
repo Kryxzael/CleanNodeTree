9d18b87959a85083611bf7faefcbe0fc8158e92b
Kryxzael <kasper.ramskjell98@gmail.com>
2018-11-08 14:44:09 +0100

    Began work on CleanNodeFormatter. Please squash this commit

---
+++ CleanNodeTree/CleanNodeFormatter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CleanNodeTree
+{
+    /// <summary>
+    /// Formatter that support serialization of a serializable object to a HierarchyNode
+    /// </summary>
+    public class CleanNodeFormatter : IFormatter
+    {
+        public ISurrogateSelector SurrogateSelector { get; set; }
+        public SerializationBinder Binder { get; set; }
+        public StreamingContext Context { get; set; }
+
+        public CleanNodeFormatter()
+        {
+            Context = new StreamingContext(StreamingContextStates.All);
+        }
+
+        public object Deserialize(Stream serializationStream)
+        {
+            //Open node from stream
+            HierarchyNode node = HierarchyNode.FromStream(serializationStream);
+
+            //Get the type of object from root name
+            Type type;
+            try
+            {
+                type = Type.GetType(node.Name);
+            }
+            catch (Exception)
+            {
+                
+            }
+            
+
+
+        }
+
+        public void Serialize(Stream serializationStream, object graph)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
